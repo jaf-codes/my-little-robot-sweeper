1cd407331e1bf27bbada1a7b9041c92ee4b4174e
Jared Fry <jafanders@hotmail.com>
2017-02-25 11:16:55 -0500

    Adding test project; continuing VS/Git exploration; adding parser object and test class

---
+++ TheLittleSweeperThatCould/TheLittleSweeperThatCould.Test/Parsing/ParserTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TheLittleSweeperThatCould.Test
+{
+    [TestClass]
+    public class ParserTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TheLittleSweeperThatCould/TheLittleSweeperThatCould.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TheLittleSweeperThatCould.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TheLittleSweeperThatCould.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("698907d7-5fc5-43eb-b579-a59a7b0c0b9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
